8fafc76360892228cc2e6162277cdb4643d9a305
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-02-25 17:38:51 +0530

    Add files via upload
    Interfaces concept added.

---
+++ 2.8 Interfaces.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+class Calculator : public AdvancedArithmetic {
+
+    int divisorSum(int n) {
+        int res=0;   
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0){
+            res=res+i;}
+        }
+        return res;
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
