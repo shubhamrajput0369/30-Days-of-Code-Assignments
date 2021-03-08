4fb6951c93e31a287326141f108fb2ac94caec10
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-08 23:57:09 +0530

    Create 3.4 More Linked List.cpp

---
+++ 3.4 More Linked List.cpp
@@ -0,0 +1,16 @@
+		
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+    head=mylist.removeDuplicates(head);
+
+	mylist.display(head);
+		
+}
