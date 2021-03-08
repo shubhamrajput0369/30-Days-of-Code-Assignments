6af7a3154eb1df3ecea531f4a727cf494dcabe78
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-08 23:58:32 +0530

    Add files via upload
    Nested Logic code added.

---
+++ 3.5 Nested Logic.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int d1,m1,y1,d2,m2,y2,fine=0,flag=0;
+    cin>>d1>>m1>>y1;
+    cin>>d2>>m2>>y2;
+    
+	    if(y1>y2)
+	    {
+	        fine = 10000;
+	        goto print;
+	    }
+	
+		else if(m1>m2 && y1==y2)
+	    {
+	        fine = 500*(m1-m2);
+	        goto print;
+	    }
+	    
+	    else if(d1>d2 && m1==m2 && y1==y2)
+	    {
+	        fine = 15*(d1-d2);
+	        goto print;
+	    }
+	    else
+	    	fine=0;
+	    
+	print: cout<<fine;
+       
+    return 0;
+}
+
+
