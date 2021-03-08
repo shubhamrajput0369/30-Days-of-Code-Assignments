b3ff139043e38bd73843513d4c34d7ea214fcff7
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-08 23:58:33 +0530

    Update 3.4 More Linked List.cpp

--- 3.4 More Linked List.cpp
+++ 3.4 More Linked List.cpp
@@ -1,4 +1,65 @@
-		
+#include <cstddef>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;	
+class Solution{
+    public:
+
+          Node* removeDuplicates(Node *head)
+          {
+            if(head==NULL || head->next==NULL)
+            return head;
+            Node* start=head;
+            while(start->next!=NULL)
+            {          
+                if(start->data==start->next->data)
+                {
+                    start->next=start->next->next;
+                }
+                else
+                start=start->next;
+            }
+            return head;
+          }
+
+          Node* insert(Node *head,int data)
+          {
+               Node* p=new Node(data);
+               if(head==NULL){
+                   head=p;  
+
+               }
+               else if(head->next==NULL){
+                   head->next=p;
+
+               }
+               else{
+                   Node *start=head;
+                   while(start->next!=NULL){
+                       start=start->next;
+                   }
+                   start->next=p;   
+
+               }
+                    return head;
+                
+            
+          }
+          void display(Node *head)
+          {
+                  Node *start=head;
+                    while(start)
+                    {
+                        cout<<start->data<<" ";
+                        start=start->next;
+                    }
+           }
+};
+	
+
 int main()
 {
 	Node* head=NULL;
