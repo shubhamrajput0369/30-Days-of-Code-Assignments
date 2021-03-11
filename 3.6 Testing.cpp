57a5461bd0fdf6ca6f31af07a8ac7d7faeb6b30b
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-11 21:30:10 +0530

    Update 3.6 Testing.cpp

--- 3.6 Testing.cpp
+++ 3.6 Testing.cpp
@@ -7,3 +7,10 @@
 #include <vector>
 #include <cassert>
 #include <set>
+
+using namespace std;
+
+int minimum_index(vector<int> seq) {
+    if (seq.empty()) {
+        throw invalid_argument("Cannot get the minimum value index from an empty sequence");
+    }
