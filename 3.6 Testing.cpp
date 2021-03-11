c42f7861c45de6ca00b04be6d8fb59fc3488656c
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-11 21:30:33 +0530

    Update 3.6 Testing.cpp

--- 3.6 Testing.cpp
+++ 3.6 Testing.cpp
@@ -14,3 +14,9 @@ int minimum_index(vector<int> seq) {
     if (seq.empty()) {
         throw invalid_argument("Cannot get the minimum value index from an empty sequence");
     }
+    int min_idx = 0;
+    for (int i = 1; i < seq.size(); ++i) {
+        if (seq[i] < seq[min_idx]) {
+            min_idx = i;
+        }
+    }
