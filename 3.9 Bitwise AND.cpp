629bd2a0c30179e6245a6a0917a852626afecff4
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-16 23:04:22 +0530

    Update 3.9 Bitwise AND.cpp

--- 3.9 Bitwise AND.cpp
+++ 3.9 Bitwise AND.cpp
@@ -1,3 +1,11 @@
 //Author: Shubham Bhagwansing Rajput
 //C++
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int ncases, n, k, max = 0, tmp = 0;
+    vector<int> range;
+    range.reserve(1000);
+ 
